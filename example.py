ee94d37dce12d501b84171edad6f76d97c6f39e0
woahbryce <36972605+woahbryce@users.noreply.github.com>
2019-12-03 18:10:57 -0600

    Updated example.py
    Updated to find and match multiple groups

--- example.py
+++ example.py
@@ -19,12 +19,14 @@ def parse(input_str):
     dictkeys_pattern = re.compile('|'.join(list(_)), re.IGNORECASE)
 
     #If string is found in the pattern, match to key in the dictionary and return corresponding value.
-    team_found = re.search(dictkeys_pattern, input_str)
+    team_found = re.findall(dictkeys_pattern, input_str)
     if team_found:
-        for k, v in nfl_teams_dict.items():
-            if re.match(k, team_found.group(), re.IGNORECASE):
-                team = v
-                return team
+        team = []
+        for i in team_found: 
+            for k, v in nfl_teams_dict.items():
+                if re.match(k, i, re.IGNORECASE):
+                    team.append(v)
+        return team
     else:
         team = None
         return team
@@ -33,18 +35,18 @@ def main():
     # Example dataframe with its possible entries, randomized
     example_list = ['New Englang Patriots', 'NE Patriots', 'Pats', 'NE pats', 'New York Buffalo Bills', 'Buffalo bills', 'NY biLls', 'Bills Mafia',
     'Green Bay Packers', 'gb packers', 'packers', 'New York Jets', 'jets', 'NY Jets', 'Minnesota Vikings', 'vikings', 'MN Viking',
-    'Philadelphia Eagles', 'Eagles', 'Philly eagles']
+    'Philadelphia Eagles', 'Eagles', 'Philly eagles', 'The New England Patriots lost to Bills Mafia!!']
     df = pd.DataFrame(np.arange(10000), columns=['team_entry']).applymap(lambda x: np.random.choice(example_list))
 
     df['teams'] = df.team_entry.apply(parse)
     '''
     print(df.head())
-                 team_entry        teams
-    0                  Pats  New England Patriots
-    1               packers     Green Bay Packers
-    2  New Englang Patriots  New England Patriots
-    3               MN Viking   Minnesota Vikings
-    4           Bills Mafia         Buffalo Bills
+                                           team_entry                                  teams
+    0                                         NE pats                 [New England Patriots]
+    1  The New England Patriots lost to Bills Mafia!!  [New England Patriots, Buffalo Bills]
+    2                             Philadelphia Eagles                  [Philadelphia Eagles]
+    3                                          Eagles                  [Philadelphia Eagles]
+    4                                       MN Viking                    [Minnesota Vikings]
     '''
 
 if __name__ == '__main__':
